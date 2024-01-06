7763571b91cb76c9d81cd73b25674503826969de
nibezo <nibezo.cs@gmail.com>
2024-01-07 00:10:24 +0400

    feat: Use current dates to the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,28 @@
 import "./Header.css";
 import Switch from "@mui/material/Switch";
 import { useState } from "react";
+import { daysOfWeek, months } from "./dates.js";
+
 export default function Header(props) {
   const [service, setService] = useState("to-do list");
   function handleServiceChange() {
     service === "to-do list" ? setService("notes") : setService("to-do list");
   }
+  const currentDate = new Date();
+  const date = currentDate.getDate();
+  const monthName = months[currentDate.getMonth()];
+  const dayName = daysOfWeek[currentDate.getDay()];
+  const year = currentDate.getFullYear();
   return (
     <>
       <header className="header">
         <div className="date">
-          <span className="date__nth">4</span>
+          <span className="date__nth">{date}</span>
           <div className="date__details">
-            <p className="date__month">JAN</p>
-            <p className="date__year">2024</p>
+            <p className="date__month">{monthName}</p>
+            <p className="date__year">{year}</p>
           </div>
-          <span className="date__day">Thursday</span>
+          <span className="date__day">{dayName}</span>
         </div>
         <div className="menu">
           <div className="service">{service}</div>
---
+++ src/components/Header/dates.js
@@ -0,0 +1,23 @@
+export const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+export const months = [
+  "JAN",
+  "FEB",
+  "MAR",
+  "APR",
+  "MAY",
+  "JUN",
+  "JUL",
+  "AUG",
+  "SEP",
+  "OCT",
+  "NOV",
+  "DEC",
+];
