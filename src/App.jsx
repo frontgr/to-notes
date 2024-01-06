e5918f1ecfe9a95c53f99bc990687c96a18073c5
nibezo <nibezo.cs@gmail.com>
2024-01-06 23:01:46 +0400

    feat: Add switch to header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 
 import Header from "./components/Header/Header.jsx";
-import Notes from "./components/Notes/Notes.jsx";
 import ToDo from "./components/ToDo/ToDo.jsx";
+import Notes from "./components/Notes/Notes.jsx";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,19 @@
+import "./Header.css";
+import Switch from "@mui/material/Switch";
+
 export default function Header(props) {
-  return <></>;
+  return (
+    <>
+      <div className="date">
+        <span className="date__nth">4</span>
+        <div className="date__details">
+          <p className="date__month">JAN</p>
+          <p className="date__year">2024</p>
+        </div>
+        <span className="date__day">Thursday</span>
+      </div>
+      <div className="service">to-do list</div>
+      <Switch />
+    </>
+  );
 }
