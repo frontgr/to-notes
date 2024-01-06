83fdb51e75ace4c6612744e515b4fe950dc444e7
nibezo <nibezo.cs@gmail.com>
2024-01-06 23:43:38 +0400

    style: Add styles for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,16 +4,20 @@ import Switch from "@mui/material/Switch";
 export default function Header(props) {
   return (
     <>
-      <div className="date">
-        <span className="date__nth">4</span>
-        <div className="date__details">
-          <p className="date__month">JAN</p>
-          <p className="date__year">2024</p>
+      <header className="header">
+        <div className="date">
+          <span className="date__nth">4</span>
+          <div className="date__details">
+            <p className="date__month">JAN</p>
+            <p className="date__year">2024</p>
+          </div>
+          <span className="date__day">Thursday</span>
         </div>
-        <span className="date__day">Thursday</span>
-      </div>
-      <div className="service">to-do list</div>
-      <Switch />
+        <div className="menu">
+          <div className="service">to-do list</div>
+          <Switch color="secondary" />
+        </div>
+      </header>
     </>
   );
 }
