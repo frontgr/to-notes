97f67c9a91a4c7e11257a0948baf473f449f2139
nibezo <nibezo.cs@gmail.com>
2024-01-07 17:02:01 +0400

    style: Add style for todo-input

--- src/components/ToDo/ToDo.jsx
+++ src/components/ToDo/ToDo.jsx
@@ -1,5 +1,18 @@
+import "./ToDo.css";
+
 export default function ToDo(props) {
-  return <>
-  
-  </>;
+  return (
+    <>
+      <div className="todo">
+        <div className="todo-input">
+          <input
+            type="text"
+            placeholder="Enter text..."
+            className="todo-input__input"
+          />
+          <button className="todo-input__btn"></button>
+        </div>
+      </div>
+    </>
+  );
 }
