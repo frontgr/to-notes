3a78741fd991bf842e8908b448400c33b9bcfcbf
nibezo <nibezo.cs@gmail.com>
2024-01-08 01:54:20 +0400

    feat: Add basic structure of todos

--- src/components/ToDo/ToDo.jsx
+++ src/components/ToDo/ToDo.jsx
@@ -10,7 +10,23 @@ export default function ToDo(props) {
             placeholder="Enter text..."
             className="todo-input__input"
           />
-          <button className="todo-input__btn"></button>
+          <button className="todo-input__add-btn"></button>
+        </div>
+        <div className="todo-item">
+          <div className="todo-item__wrapper">
+            <div className="todo-item__checkbox"></div>
+            <p className="todo-item__text">Do homework</p>
+          </div>
+          <button className="todo-item__close-btn"></button>
+        </div>
+        <div className="todo-item">
+          <div className="todo-item__wrapper">
+            <div className="todo-item__checkbox todo-item__checkbox--checked"></div>
+            <p className="todo-item__text todo-item__text--checked">
+              Do another task
+            </p>
+          </div>
+          <button className="todo-item__close-btn"></button>
         </div>
       </div>
     </>
